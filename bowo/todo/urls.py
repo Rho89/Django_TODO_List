cae8e4a5cfe84fd761ec6d413dc3f7f9416be457
Romain Huet <romain.huet@epitech.eu>
2018-03-28 16:41:37 +0200

    working on login

--- bowo/todo/urls.py
+++ bowo/todo/urls.py
@@ -2,7 +2,12 @@
 from django.conf.urls import url
 from todo import views
 
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
+    url(r'^about/$', views.AboutPageView.as_view()),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
--- bowo/todo/views.py
+++ bowo/todo/views.py
@@ -3,6 +3,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
 # Create your views here.
 
 class HomePageView(TemplateView):
